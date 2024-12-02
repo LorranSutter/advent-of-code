e73e889ca4cf0e06c2f5e08d03ddf963991d188b
Lorran Sutter <sutterlorran@gmail.com>
2024-12-02 11:18:08 -0500

    missing code

--- 2024/day01/main.py
+++ 2024/day01/main.py
@@ -4,18 +4,39 @@ script_dir = os.path.dirname(__file__)
 rel_path = "input"
 abs_file_path = os.path.join(script_dir, rel_path)
 
-left, right = [], []
-with open(abs_file_path) as f:
-    for line in f:
-        line = list(map(int, line.split()))
-        left.append(line[0])
-        right.append(line[1])
-        
-    left.sort()
-    right.sort()
+def part1():
+    left, right = [], []
+    with open(abs_file_path) as f:
+        for line in f:
+            line = list(map(int, line.split()))
+            left.append(line[0])
+            right.append(line[1])
+            
+        left.sort()
+        right.sort()
 
-    s = 0
-    for l, r in zip(left, right):
-        s += abs(l-r)
-    
-    print('Total distance: ', s)
+        s = 0
+        for l, r in zip(left, right):
+            s += abs(l-r)
+        
+        print('Total distance: ', s)
+
+def part2():
+    left = []
+    count_right = {}
+    with open(abs_file_path) as f:
+        for line in f:
+            line = list(map(int,line.split()))
+            left.append(line[0])
+
+            if line[1] not in count_right:
+                count_right[line[1]] = 1
+            else:
+                count_right[line[1]] += 1
+
+        s = 0
+        for l in left:
+            if l in count_right:
+                s += l * count_right[l]
+        
+        print('Similarity score: ', s)
