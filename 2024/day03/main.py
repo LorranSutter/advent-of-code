07eef71f2ee11e4ec7b3314f2e85a391a5f62fab
Lorran Sutter <sutterlorran@gmail.com>
2024-12-03 00:27:01 -0500

    day 03

---
+++ 2024/day03/main.py
@@ -0,0 +1,44 @@
+import os
+import re
+
+script_dir = os.path.dirname(__file__)
+rel_path = "input"
+abs_file_path = os.path.join(script_dir, rel_path)
+
+
+def part1():
+    s = 0
+    with open(abs_file_path) as f:
+        memory = f.read()
+        pairs = re.findall("mul\((\d+),(\d+)\)", memory)
+
+        for pair in pairs:
+            s += int(pair[0]) * int(pair[1])
+
+    print("Sum:", s)
+
+
+def part2():
+    s = 0
+    with open(abs_file_path) as f:
+        memory = f.read()
+        words = re.findall("mul\(\d+,\d+\)|do\(\)|don't\(\)", memory)
+
+        enabled = True
+        for word in words:
+            if word == "do()":
+                enabled = True
+            elif word == "don't()":
+                enabled = False
+            elif enabled:
+                s += multiply(word)
+
+    print("Sum:", s)
+
+
+def multiply(op):
+    nums = re.findall("\d+", op)
+    return int(nums[0]) * int(nums[1])
+
+
+part2()
