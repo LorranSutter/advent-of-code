d0f15469e740102d4006a4985f2bf451f58bfc27
Lorran Sutter <sutterlorran@gmail.com>
2024-12-05 01:37:22 -0500

    day 5 part 2

--- 2024/day05/main.py
+++ 2024/day05/main.py
@@ -11,30 +11,50 @@ def part1():
     with open(abs_file_path) as f:
         # Rules
         for line in f:
-            if line == '\n':
+            if line == "\n":
                 break
-            line = line.rstrip('\n').split('|')
+            line = line.rstrip("\n").split("|")
             if line[0] in rules:
                 rules[line[0]].append(line[1])
             else:
                 rules[line[0]] = [line[1]]
-        
+
         s_mid_pages = 0
         for updates in f:
-            pages = updates.rstrip('\n').split(',')
+            pages = updates.rstrip("\n").split(",")
             if isCorrectlyOrdered(rules, pages):
-                s_mid_pages += int(pages[len(pages)//2])
+                s_mid_pages += int(pages[len(pages) // 2])
             print()
 
     print("Sum middle pages:", s_mid_pages)
 
 
 def part2():
-    pass
+    rules = {}
+    updates = []
+    with open(abs_file_path) as f:
+        # Rules
+        for line in f:
+            if line == "\n":
+                break
+            line = line.rstrip("\n").split("|")
+            if line[0] in rules:
+                rules[line[0]].append(line[1])
+            else:
+                rules[line[0]] = [line[1]]
+
+        s_mid_pages = 0
+        for updates in f:
+            pages = updates.rstrip("\n").split(",")
+            ordered_pages = orderIfNeeded(rules, pages.copy(), 0, len(pages))
+            if pages != ordered_pages:
+                s_mid_pages += int(ordered_pages[len(ordered_pages) // 2])
+
+    print("Sum middle pages:", s_mid_pages)
 
 
 def isCorrectlyOrdered(rules, pages):
-    for i, first_page in enumerate(pages[:-1],1):
+    for i, first_page in enumerate(pages[:-1], 1):
         if first_page not in rules:
             return False
         for page_after in pages[i:]:
@@ -42,4 +62,26 @@ def isCorrectlyOrdered(rules, pages):
                 return False
     return True
 
-part1()
+
+def orderIfNeeded(rules, pages, index, size):
+    if index >= size - 1:
+        return pages
+
+    for i in range(index, size - 1):
+        if pages[i] not in rules:
+            pushToEnd(pages, i)
+            return orderIfNeeded(rules, pages, i, size)
+        for page_after in pages[i + 1 :]:
+            if page_after not in rules[pages[i]]:
+                pushToEnd(pages, i)
+                return orderIfNeeded(rules, pages, i, size)
+
+    return pages
+
+
+def pushToEnd(l, index):
+    item = l.pop(index)
+    l.append(item)
+
+
+part2()
