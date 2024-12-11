5cafa31eba5971cc07955f46373bb97e0e593f47
Lorran Sutter <sutterlorran@gmail.com>
2024-12-11 00:48:58 -0500

    small optimization

--- 2024/day11/main.py
+++ 2024/day11/main.py
@@ -7,20 +7,20 @@ abs_file_path = os.path.join(script_dir, rel_path)
 
 def part1():
     arr = read_file()
-
+    
     temp = []
     for _ in range(25):
         for i in range(len(arr)):
-            if arr[i] == "0":
-                temp.append("1")
+            if arr[i] == 0:
+                temp.append(1)
             else:
-                size = len(arr[i])
+                size = len(str(arr[i]))
                 if size % 2 == 0:
-                    stone1 = str(int(arr[i][: size // 2]))
-                    stone2 = str(int(arr[i][size // 2 :]))
+                    stone1 = int(str(arr[i])[: size // 2])
+                    stone2 = int(str(arr[i])[size // 2 :])
                     temp.extend([stone1, stone2])
                 else:
-                    temp.append(str(int(arr[i]) * 2024))
+                    temp.append(arr[i] * 2024)
         arr = temp.copy()
         temp = []
 
@@ -28,13 +28,14 @@ def part1():
 
 
 def part2():
+    arr = read_file()
 
-    print("Total stones:")
+    print("Total stones:", len(arr))
 
 
 def read_file():
     with open(abs_file_path) as f:
-        return f.read().rstrip("\n").split()
+        return list(map(int, f.read().rstrip("\n").split()))
 
 
 part1()
