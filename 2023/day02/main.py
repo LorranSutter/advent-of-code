bf7b57a0fa908f1f4e33eefdd97a8ced5dcacad4
Lorran Sutter <sutterlorran@gmail.com>
2025-10-05 00:30:42 -0400

    day 02

---
+++ 2023/day02/main.py
@@ -0,0 +1,75 @@
+import os
+
+script_dir = os.path.dirname(__file__)
+rel_path = "input.txt"
+abs_file_path = os.path.join(script_dir, rel_path)
+
+
+def part1():
+    games = read_file()
+
+    red_limit, green_limit, blue_limit = 12, 13, 14
+
+    valid_games = 0
+    for i, game in enumerate(games, 1):
+        valid_games += i
+        for set_game in game:
+            if (
+                set_game[0] > red_limit
+                or set_game[1] > green_limit
+                or set_game[2] > blue_limit
+            ):
+                valid_games -= i
+                break
+
+    print("Sum possible games:", valid_games)
+
+
+def part2():
+    games = read_file()
+
+    total_power = 0
+    for game in games:
+        min_red, min_green, min_blue = 0, 0, 0
+        for set_game in game:
+            if set_game[0] > min_red:
+                min_red = set_game[0]
+            if set_game[1] > min_green:
+                min_green = set_game[1]
+            if set_game[2] > min_blue:
+                min_blue = set_game[2]
+
+        total_power += min_red * min_green * min_blue
+
+    print("Total game power:", total_power)
+
+
+def read_file():
+    games = []
+    with open(abs_file_path) as f:
+        for line in f:
+            line = line.rstrip("\n").split(":")
+            line = line[1]  # only games
+            line_games = line.split(";")  # separate games
+
+            new_set_game = []
+            for game in line_games:
+                game = game.split(",")
+                new_game = [0 for _ in range(3)]
+                for cubes in game:
+                    cubes = cubes.strip().split(" ")
+                    if cubes[1] == "red":
+                        new_game[0] = int(cubes[0])
+                    if cubes[1] == "green":
+                        new_game[1] = int(cubes[0])
+                    if cubes[1] == "blue":
+                        new_game[2] = int(cubes[0])
+
+                new_set_game.append(new_game)
+
+            games.append(new_set_game)
+
+    return games
+
+
+part2()
