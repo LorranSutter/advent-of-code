ef1cc29190e2ee9254f297ce8afa9adce94b8785
LorranSutter <sutterlorran@gmail.com>
2026-05-08 21:11:08 -0400

    2023 day 11 part 2

--- 2023/day11/main.py
+++ 2023/day11/main.py
@@ -4,7 +4,28 @@ from typing import List, Tuple
 from utils.timer import timer
 
 """
-Explanation
+Preprocessing:
+- Just read the input file and parse into a 2D list
+
+Part 1:
+- The trick here is to realize that the shortest path between two galaxies is just the Manhattan distance:
+    - Euclidian distance: sqrt((x2-x1)^2 + (y2-y1)^2)
+    - Manhattan distance: abs(x2-x1) + abs(y2-y1)
+- I used the naive approach to expand the universe by adding more rows and columns of empty cells (function expand_universe)
+- Finally, we just have to iterate over each pair of galaxies, calculate the Manhattan distance and add it to the total
+
+Part 2:
+- The part 2 was quite predictable, same idea as part 1, but with a way larger expanded universe
+- The "aha moment" here is to realize the universe expansion is pretty much a linear transformation
+- So, if we know how much the universe expanded, we can perform the same process of part 1 plus the expansion size
+- We changed the "expande_universe" function
+    - Accepts the size of the expansion and return which rows and columns were expanded
+    - Not changing the universe matrix itself, so we can expanded how much we want without worrying about the memory usage
+- Then, we iterate over each pair of galaxies, get how many rows and columns were between each pair with the function "get_expansions_between_galaxies"
+- Finally, we calculate the Manhattan distance with the offset of the expansion:
+    - x_path = abs(x2-x1) + num_rows + expansion_size
+    - y_path = abs(y2-y1) + num_cols + expansion_size
+    - total += x_path + y_path
 """
 
 
@@ -12,105 +33,96 @@ Explanation
 def part1():
     universe = parse_file("input.txt")
 
-    expand_universe(universe, 1, True)
+    expand_universe(universe, 2, True)
     print_universe(universe)
 
-    galaxies = []
-    for i in range(len(universe)):
-        for j in range(len(universe[i])):
-            if universe[i][j] == "#":
-                galaxies.append((i, j))
+    galaxies = get_list_of_galaxies(universe)
 
     sum_shortest_paths = 0
     for i in range(len(galaxies)):
         for j in range(i + 1, len(galaxies)):
-            sum_shortest_paths += abs(galaxies[i][0] - galaxies[j][0]) + abs(
-                galaxies[i][1] - galaxies[j][1]
-            )
+            x_path = abs(galaxies[i][0] - galaxies[j][0])
+            y_path = abs(galaxies[i][1] - galaxies[j][1])
+
+            sum_shortest_paths += x_path + y_path
 
     print("Sum of shortest paths:", sum_shortest_paths)
 
 
 @timer
 def part2():
-    universe = parse_file("input_sample.txt")
-    expansion_size = 10
+    universe = parse_file("input.txt")
+    expansion_size = 1000000
 
-    row_expansions, col_expansions = expand_universe(universe, expansion_size, True)
-    print(row_expansions, col_expansions)
+    row_expansions, col_expansions = expand_universe(universe, expansion_size, False)
+    galaxies = get_list_of_galaxies(universe)
 
-    galaxies = []
-    for i in range(len(universe)):
-        for j in range(len(universe[i])):
-            if universe[i][j] == "#":
-                galaxies.append((i, j))
-    
     print_universe(universe)
-    
+
+    expansion_size -= 1
     sum_shortest_paths = 0
-    count_row_expanded_galaxies = 0
-    count_col_expanded_galaxies = 0
     for i in range(len(galaxies)):
         for j in range(i + 1, len(galaxies)):
-            rows_between, cols_between = get_expansions_between_galaxies(galaxies[i], galaxies[j], row_expansions, col_expansions)
-            # print(f"Expansions {galaxies[i]} and {galaxies[j]}: {rows_between} {cols_between}")
+            num_rows, num_cols = get_expansions_between_galaxies(
+                galaxies[i], galaxies[j], row_expansions, col_expansions
+            )
 
-            if len(rows_between) > 0:
-                count_row_expanded_galaxies += 1
-            if len(cols_between) > 0:
-                count_col_expanded_galaxies += 1
-            
-            # x_path = abs(galaxies[i][0] - galaxies[j][0]) + len(rows_between) * (expansion_size-1)
-            # y_path = abs(galaxies[i][1] - galaxies[j][1]) + len(cols_between) * (expansion_size-1)
+            x_path = abs(galaxies[i][0] - galaxies[j][0]) + num_rows * expansion_size
+            y_path = abs(galaxies[i][1] - galaxies[j][1]) + num_cols * expansion_size
 
-            # print(f"Path between {galaxies[i]} and {galaxies[j]}: x_path={x_path}, y_path={y_path}")
+            sum_shortest_paths += x_path + y_path
 
-            # sum_shortest_paths += x_path + y_path
-
-            path = abs(galaxies[i][0] - galaxies[j][0]) + abs(galaxies[i][1] - galaxies[j][1])
-            print(f"Expansions {str(galaxies[i]):9} and {str(galaxies[j]):9}: {str(rows_between):8} {str(cols_between):12}, path={path}")
-            sum_shortest_paths += path
-
-    print(f"Count total row expanded galaxies: {count_row_expanded_galaxies}")
-    print(f"Count total column expanded galaxies: {count_col_expanded_galaxies}")
-    print(f"Count total expanded galaxies: {count_row_expanded_galaxies + count_col_expanded_galaxies}")
     print("Sum of shortest paths:", sum_shortest_paths)
 
 
 def expand_universe(
     universe: List[Tuple[str]], expansion_size: int, update_universe: bool = False
 ) -> Tuple[int, int]:
+    """
+    Calculate the universe expanded by the given expansion size.
+    If update_universe is True, the universe will be updated with the new rows and columns.
+    """
     if expansion_size <= 1:
         return [], []
-    
+    expansion_size -= 1
+
     rows_to_expand = []
     for i in range(len(universe)):
         if all(cell == "." for cell in universe[i]):
             if update_universe:
-                rows_to_expand.append(i + len(rows_to_expand) * (expansion_size - 1))
+                rows_to_expand.append(i + len(rows_to_expand) * expansion_size)
             else:
                 rows_to_expand.append(i)
 
     if update_universe:
         for i in rows_to_expand:
-            for j in range(expansion_size-1):
-                universe.insert(i+j, tuple("." for _ in range(len(universe[0]))))
+            for j in range(expansion_size):
+                universe.insert(i + j, tuple("." for _ in range(len(universe[0]))))
 
     cols_to_expand = []
     for j in range(len(universe[0])):
         if all(universe[i][j] == "." for i in range(len(universe))):
             if update_universe:
-                cols_to_expand.append(j + len(cols_to_expand) * (expansion_size - 1))
+                cols_to_expand.append(j + len(cols_to_expand) * expansion_size)
             else:
                 cols_to_expand.append(j)
 
     if update_universe:
         for j in cols_to_expand:
-            for k in range(expansion_size-1):
+            for k in range(expansion_size):
                 for i in range(len(universe)):
-                    universe[i] = universe[i][:j+k] + (".",) + universe[i][j+k:]
+                    universe[i] = universe[i][: j + k] + (".",) + universe[i][j + k :]
 
     return rows_to_expand, cols_to_expand
+
+
+def get_list_of_galaxies(universe: List[Tuple[str]]) -> List[Tuple[int, int]]:
+    galaxies = []
+    for i in range(len(universe)):
+        for j in range(len(universe[i])):
+            if universe[i][j] == "#":
+                galaxies.append((i, j))
+    return galaxies
 
 
 def get_expansions_between_galaxies(
@@ -118,20 +130,23 @@ def get_expansions_between_galaxies(
     galaxy2: Tuple[int, int],
     row_expansions: List[int],
     col_expansions: List[int],
-) -> Tuple[List[int], List[int]]:
+) -> Tuple[int, int]:
+    """
+    Returns the lists of rows and columns that are expandable between two galaxies
+    """
     x1, x2 = sorted([galaxy1[0], galaxy2[0]])
     y1, y2 = sorted([galaxy1[1], galaxy2[1]])
 
-    rows_between = []
+    rows_between = 0
     for row in row_expansions:
         if x1 < row < x2:
-            rows_between.append(row)
-    
-    cols_between = []
+            rows_between += 1
+
+    cols_between = 0
     for col in col_expansions:
         if y1 < col < y2:
-            cols_between.append(col)
-    
+            cols_between += 1
+
     return rows_between, cols_between
 
 
@@ -151,63 +166,5 @@ def parse_file(file_name: str) -> List[Tuple[str]]:
     return universe
 
 
-# part1()
+part1()
 part2()
-
-
-# .....#..........
-# ...........#....
-# #...............
-# ................
-# ................
-# ................
-# ..........#.....
-# .#..............
-# ...............#
-# ................
-# ................
-# ................
-# ...........#....
-# #.....#.........
-
-# 2 5 8
-# ....#.......
-# .........#..
-# #...........
-# ............
-# ........#...
-# .#..........
-# ...........#
-# ............
-# .........#..
-# #....#......
-
-# .....#..........
-# ...........#....
-# #...............
-# ................
-# ................
-# ................
-# ..........#.....
-# .#..............
-# ...............#
-# ................
-# ................
-# ................
-# ...........#....
-# #.....#.........
-
-# .....#..........
-# .............#..
-# #...............
-# ................
-# ................
-# ................
-# ............#...
-# .#..............
-# ................
-# ................
-# ...............#
-# ................
-# .............#..
-# #.......#.......
